5cf1eb1d0ee4a259aef584b20c99e515cd17ea5b
yangjinjie <yangjinjie@laiye.com>
2019-08-19 11:33:53 +0800

    update Dockerfile

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-bert-serving-start -num_worker=$1 -model_dir /model
+bert-serving-start -model_dir chinese_L-12_H-768_A-12 -num_worker=2 -port 2400 -port_out 2401 -http_port 8125 -http_max_connect 800 -xla -fp16
